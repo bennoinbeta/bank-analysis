4b163e97510c84ca8241121966290b07a20d0dae
Benno Kohrs <bennokohrs@gmail.com>
2021-10-09 12:54:21 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useDropzone } from 'react-dropzone';
+import { onDrop } from './core';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
